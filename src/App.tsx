b0df0638b5d8d6055a201c959d13fdc625e03b94
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-06-03 14:18:43 +0530

    Added shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div className="bg-red-500">
+      <Button className="bg-red-500">Hello</Button>
+    </div>
+  )
+}
